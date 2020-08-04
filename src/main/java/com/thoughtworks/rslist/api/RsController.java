56830fa740191588a0c72818e85a972abda6cc66
yurong <rong.yu@thoughtworks.com>
2020-08-04 19:35:28 +0800

    refactor: get_rsevent_list

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,17 +1,34 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.domain.RsEvent;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = initRsEventList();
+
+  private List<RsEvent> initRsEventList(){
+    List<RsEvent> rsEventList = new ArrayList<>();
+    rsEventList.add(new RsEvent("第一条事件", "无标签"));
+    rsEventList.add(new RsEvent("第二条事件", "无标签"));
+    rsEventList.add(new RsEvent("第三条事件", "无标签"));
+    return rsEventList;
+  }
+  
 
   @GetMapping("/rs/list")
-  public String getRsEventList(){
-    return rsList.toString();
+  public List<RsEvent> getRsEventBetween(@RequestParam(required = false) Integer start,
+                                         @RequestParam(required = false) Integer end){
+    if (start != null && end != null)
+      return this.rsList.subList(start-1, end);
+    return this.rsList;
   }
+
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -18,10 +20,16 @@ class RsListApplicationTests {
     MockMvc mockMvc;
 
     @Test
-    void contextLoads() throws Exception {
+    public void get_rs_event_list() throws Exception {
         mockMvc.perform(get("/rs/list"))
-                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyword", is("无标签")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyword", is("无标签")))
+                .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[2].keyword", is("无标签")));
     }
 
 }
