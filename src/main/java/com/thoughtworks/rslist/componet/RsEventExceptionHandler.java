e52565606332c56367d6937c3fb16715d09ac6d0
yurong <rong.yu@thoughtworks.com>
2020-08-05 23:04:15 +0800

    [yurong] add log

--- src/main/java/com/thoughtworks/rslist/componet/RsEventExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/componet/RsEventExceptionHandler.java
@@ -3,14 +3,20 @@ package com.thoughtworks.rslist.componet;
 import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.exception.Error;
 import com.thoughtworks.rslist.exception.RsEventIndexNotValidException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 @ControllerAdvice
 public class RsEventExceptionHandler {
+
+    private static final String logExceptionFormat = "Capture Exception By GlobalExceptionHandler - Detail: %s";
+    private static Logger log = LoggerFactory.getLogger(RsEventExceptionHandler.class);
 
     @ExceptionHandler({RsEventIndexNotValidException.class, MethodArgumentNotValidException.class})
     public ResponseEntity RsEventExceptionHandler(Exception e){
@@ -27,6 +33,7 @@ public class RsEventExceptionHandler {
         Error error = new Error();
         error.setError(message);
 
+        log.error(String.format(logExceptionFormat,message));
         return ResponseEntity.badRequest().body(error);
     }
 
@@ -37,4 +44,6 @@ public class RsEventExceptionHandler {
 
         return ResponseEntity.badRequest().body(error);
     }
+
+
 }
