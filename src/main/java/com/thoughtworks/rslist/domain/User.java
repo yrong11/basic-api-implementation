2a7ac40a2a511b8264614813961aa08d64697d28
yurong <rong.yu@thoughtworks.com>
2020-08-05 11:25:14 +0800

    [yurong] feat: user register and test

---
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -0,0 +1,79 @@
+package com.thoughtworks.rslist.domain;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+
+@Data
+public class User {
+    @NotNull @Size(max = 8)
+    private String name;
+    @NotNull
+    private String gender;
+    @NotNull @Min(18) @Max(100)
+    private int age;
+    @Email
+    private String email;
+    @Pattern(regexp = "1\\d{10}")
+    private String phone;
+    private int voteNum = 10;
+
+    public User(){
+
+    }
+    public User(String name, String gender, int age, String email, String phone) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setVoteNum(int voteNum) {
+        this.voteNum = voteNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getVoteNum() {
+        return voteNum;
+    }
+}
---
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -0,0 +1,97 @@
+package com.thoughtworks.rslist.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_register_user() throws Exception {
+        User user = new User("yurong", "femal", 23, "yurong@gmail.com", "11321111111");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void name_should_not_null() throws Exception {
+        User user = new User("", "femal", 23, "yurong@gmail.com", "1321111111");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void name_size_should_less_than_8() throws Exception {
+        User user = new User("yurongyur", "femal", 23, "yurong@gmail.com", "1321111111");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void age_should_between_18_and_100() throws Exception {
+        User user = new User("yurong", "femal", 11, "yurong@gmail.com", "1321111111");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void email_should_conform_specification_format() throws Exception {
+        User user = new User("yurong", "femal", 19, "yurong.com", "1321111111");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void phone_length_should_be_11() throws Exception {
+        User user = new User("yurong", "femal", 19, "yurong@gmail.com", "132111111");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void phone_should_begin_with_1() throws Exception {
+        User user = new User("yurong", "femal", 19, "yurong@gmail.com", "3321111101");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+
+
+
+}
